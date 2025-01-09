4cade7653d5aa46a42b60467239c7366d18a658d
muhammad <mdrehanuzzaman80@gmail.com>
2025-01-09 16:58:24 +0530

    sum

---
+++ sum.js
@@ -0,0 +1,23 @@
+// You are given a number N. Print sum of all even numbers from 1 to N.
+
+// let n=5;
+// let sum=0;
+
+// for(let i=1; i<=n; i++){
+
+//     if(i%2==1){
+//        // console.log(i)
+//        sum=sum+i;
+//     }
+// } console.log(sum)
+
+
+//let alphabt = "A"
+for(let i=5; i>=1; i--){
+  let chr = ""
+   for(let j=5; j>=i;j--){
+     chr = chr + j;
+   }
+   console.log(chr);
+   
+ } 
